6979c337266e8f7f8f63356b4793aaed15a5aacf
Alex Wohletz <awohletz3@gmail.com>
2024-08-08 12:50:35 -0600

    basic model working

---
+++ dbt/pylotlight/models/example/base_data.sql
@@ -0,0 +1,11 @@
+with source as (
+      SELECT 
+    sepal_length,
+    sepal_width,
+    petal_length,
+    petal_width,
+    species
+FROM {{ source('file_source', 'iris_data') }}
+)
+select * from source
+  
